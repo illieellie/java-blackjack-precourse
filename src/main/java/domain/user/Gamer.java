c2e79aefd96105e7c9f5fceeb24e41db530232be
imellie <ssul4666@naver.com>
2024-06-20 10:07:17 +0900

    docs: write a readme

---
+++ src/main/java/domain/user/Gamer.java
@@ -0,0 +1,23 @@
+package domain.user;
+
+import domain.card.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gamer {
+    final List<Card> cards = new ArrayList<>();
+
+    // 게이머들은 아래와 같은 공통 기능을 갖는다
+    // 카드를 갖는다.
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    // 카드를 체크한다.
+
+    public void checkCard(){
+        // 자신의 카드 체크
+        // this.cards
+    }
+}
